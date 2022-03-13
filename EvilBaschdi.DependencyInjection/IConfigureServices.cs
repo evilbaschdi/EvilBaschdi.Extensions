bcac215b3db9014fb0501b553b0409af2dfb946e
Sebastian Walter <sebwalter@gmx.net>
2022-03-13 11:36:32 +0100

    corrected IConfigureServices

--- EvilBaschdi.DependencyInjection/IConfigureServices.cs
+++ EvilBaschdi.DependencyInjection/IConfigureServices.cs
@@ -1,10 +1,11 @@
 using EvilBaschdi.Core;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace EvilBaschdi.DependencyInjection;
 
 /// <inheritdoc />
 // ReSharper disable once UnusedType.Global
-public interface IConfigureServices : IRunFor<IServiceCollection>
+public interface IConfigureServices : IRunFor2<HostBuilderContext, IServiceCollection>
 {
 }
