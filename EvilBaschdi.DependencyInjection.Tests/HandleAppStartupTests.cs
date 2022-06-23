bbd38d37ceb104524325f478b7ee348d31d7cfcf
Sebastian Walter <sebwalter@gmx.net>
2022-06-23 19:09:52 +0200

    - adjusted DocumentationFile configuration - raised sdk version - changed files to file-scoped namespaces - defined global usings

--- EvilBaschdi.DependencyInjection.Tests/HandleAppStartupTests.cs
+++ EvilBaschdi.DependencyInjection.Tests/HandleAppStartupTests.cs
@@ -1,28 +1,22 @@
-﻿using AutoFixture.Idioms;
-using EvilBaschdi.Testing;
-using FluentAssertions;
-using Xunit;
+﻿namespace EvilBaschdi.DependencyInjection.Tests;
 
-namespace EvilBaschdi.DependencyInjection.Tests
+public class HandleAppStartupTests
 {
-    public class HandleAppStartupTests
+    [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+    public void Constructor_HasNullGuards(GuardClauseAssertion assertion)
     {
-        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
-        public void Constructor_HasNullGuards(GuardClauseAssertion assertion)
-        {
-            assertion.Verify(typeof(HandleAppStartup<string>).GetConstructors());
-        }
+        assertion.Verify(typeof(HandleAppStartup<string>).GetConstructors());
+    }
 
-        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
-        public void Constructor_ReturnsInterfaceName(HandleAppStartup<string> sut)
-        {
-            sut.Should().BeAssignableTo<IHandleAppStartup<string>>();
-        }
+    [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+    public void Constructor_ReturnsInterfaceName(HandleAppStartup<string> sut)
+    {
+        sut.Should().BeAssignableTo<IHandleAppStartup<string>>();
+    }
 
-        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
-        public void Methods_HaveNullGuards(GuardClauseAssertion assertion)
-        {
-            assertion.Verify(typeof(HandleAppStartup<string>).GetMethods().Where(method => !method.IsAbstract));
-        }
+    [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+    public void Methods_HaveNullGuards(GuardClauseAssertion assertion)
+    {
+        assertion.Verify(typeof(HandleAppStartup<string>).GetMethods().Where(method => !method.IsAbstract));
     }
 }
--- EvilBaschdi.DependencyInjection.Tests/HostInstanceTests.cs
+++ EvilBaschdi.DependencyInjection.Tests/HostInstanceTests.cs
@@ -1,28 +1,22 @@
-﻿using AutoFixture.Idioms;
-using EvilBaschdi.Testing;
-using FluentAssertions;
-using Xunit;
+﻿namespace EvilBaschdi.DependencyInjection.Tests;
 
-namespace EvilBaschdi.DependencyInjection.Tests
+public class HostInstanceTests
 {
-    public class HostInstanceTests
+    [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+    public void Constructor_HasNullGuards(GuardClauseAssertion assertion)
     {
-        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
-        public void Constructor_HasNullGuards(GuardClauseAssertion assertion)
-        {
-            assertion.Verify(typeof(HostInstance).GetConstructors());
-        }
+        assertion.Verify(typeof(HostInstance).GetConstructors());
+    }
 
-        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
-        public void Constructor_ReturnsInterfaceName(HostInstance sut)
-        {
-            sut.Should().BeAssignableTo<IHostInstance>();
-        }
+    [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+    public void Constructor_ReturnsInterfaceName(HostInstance sut)
+    {
+        sut.Should().BeAssignableTo<IHostInstance>();
+    }
 
-        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
-        public void Methods_HaveNullGuards(GuardClauseAssertion assertion)
-        {
-            assertion.Verify(typeof(HostInstance).GetMethods().Where(method => !method.IsAbstract));
-        }
+    [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+    public void Methods_HaveNullGuards(GuardClauseAssertion assertion)
+    {
+        assertion.Verify(typeof(HostInstance).GetMethods().Where(method => !method.IsAbstract));
     }
 }
