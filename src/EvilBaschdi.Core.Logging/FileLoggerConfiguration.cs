d2ecaeeaf2d54aa59af6250534d7e5b39becf54b
Sebastian Walter <sebwalter@gmx.net>
2026-06-04 12:43:54 +0200

    stupid me restored already cleaned up files.

--- src/EvilBaschdi.Core.Logging/FileLoggerConfiguration.cs
+++ src/EvilBaschdi.Core.Logging/FileLoggerConfiguration.cs
@@ -122,18 +122,3 @@ public class FileLoggerConfiguration
 #pragma warning restore CA2000
     }
 }
-
-/// <summary>
-///     Configuration for file-based logging.
-///     Uses the generic <see cref="FileLoggerConfiguration" /> internally.
-/// </summary>
-public static class ConfigureFileLogger
-{
-    /// <summary>
-    ///     Configures file-based logging for the watcher using sensible defaults.
-    /// </summary>
-    public static void AddFileLoggerConfiguration(this ILoggingBuilder builder, FileLoggerConfiguration config)
-    {
-        config.Configure(builder);
-    }
-}
